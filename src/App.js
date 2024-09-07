fad01ea7e83862e219be4d55fb607e0bf97caab0
Ashita <ashitam2407@gmail.com>
2024-09-07 20:21:38 +0530

    adding basic structure and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./index.css";
+let array = [
+  { key: "1", Description: "Dissatisfied", percent: 0 },
+  { key: "2", Description: "It was ok", percent: 5 },
+  { key: "3", Description: "It was good", percent: 10 },
+  { key: "4", Description: "Absolutely Amazing", percent: 5 },
+];
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [service, setService] = useState(0);
+  const [friend, setFriend] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill bill={bill} setBill={setBill} />
+      <Services array={array} service={service} setService={setService} />
+      <FriendS array={array} />
+      <Pay bill={bill} />
     </div>
   );
 }
 
-export default App;
+function Bill({ bill, setBill }) {
+  return (
+    <div>
+      <strong>How much was the Bill?</strong>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
+    </div>
+  );
+}
+function Services({ array, service, setService }) {
+  return (
+    <div>
+      <strong>How much did you like the service? </strong>
+      <select name="dropdown" id="dropdown">
+        {array.map((a) => (
+          <Option a={a} />
+        ))}
+      </select>
+    </div>
+  );
+}
+function Option({ a }) {
+  return (
+    <option value="">
+      {a.Description} {a.percent}%
+    </option>
+  );
+}
+
+function FriendS() {
+  return (
+    <div>
+      <strong>How much did your friend like the service? </strong>
+      <select name="dropdown" id="dropdown">
+        {array.map((a) => (
+          <Option a={a} />
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function Pay({ bill }) {
+  return (
+    <h2>
+      <b>You pay ${bill} ($xx + $yy) </b>
+    </h2>
+  );
+}
